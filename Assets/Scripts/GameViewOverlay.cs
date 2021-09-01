a8fc3156f6302b391baf715895e1c8e3cfd39b1b
Dave Calderwood <davidhcalderwood@gmail.com>
2021-09-01 10:30:07 -0400

    HP and XP bar setup

--- Assets/Scripts/GameViewOverlay.cs
+++ Assets/Scripts/GameViewOverlay.cs
@@ -8,7 +8,7 @@ public class GameViewOverlay : MonoBehaviour
 {
     public static GameViewOverlay instance;
     public Text hpText, lvlText, expText;
-    public Slider expSlider;
+    public Slider expSlider, hpSlider;
     private PlayerStats playerOverlay;
     public GameObject charStatHolder;
 
@@ -34,14 +34,19 @@ public class GameViewOverlay : MonoBehaviour
         }
         else
         {
-
+            // EXP Bar
             expText.text = "" + playerOverlay.currentEXP + "/" + playerOverlay.expToNextLevel[playerOverlay.playerLevel];
             expSlider.maxValue = playerOverlay.expToNextLevel[playerOverlay.playerLevel];
             expSlider.value = playerOverlay.currentEXP;
 
+            // HP Bar
+            hpText.text = "" + playerOverlay.currentHP + "/" + playerOverlay.maxHP;
+            hpSlider.maxValue = playerOverlay.maxHP;
+            hpSlider.value = playerOverlay.currentHP;
+
             lvlText.text = "" + playerOverlay.playerLevel;
 
-            hpText.text = "HP: " + playerOverlay.currentHP + "/" + playerOverlay.maxHP;
+            // hpText.text = "HP: " + playerOverlay.currentHP + "/" + playerOverlay.maxHP;
         }
     }
 }
