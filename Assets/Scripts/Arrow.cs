f40538716c2b5999a9f0682c3f074e621c0609e7
Dave Calderwood <davidhcalderwood@gmail.com>
2021-10-09 18:38:58 -0400

    Freeze arrow on collision

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    public Rigidbody2D rb;
     void Update()
     {
         Object.Destroy(gameObject, 2.0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
     }
 }
